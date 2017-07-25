fc2c503e3cc6a394411ce2974d8d2dbd03613d86
Mohammed Rizvi <morizvi07@gmail.com>
2017-07-24 22:18:52 -0400

    Worked on the recipe detail list and shopping list components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.modal';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [] = [
+    new Recipe('A Test Recipe', 'This is a simply a test', 'http://del.h-cdn.co/assets/16/04/parmesan-crusted-shrimp-quesadillas1.jpg')
+  ];
+
   constructor() { }
 
   ngOnInit() {
